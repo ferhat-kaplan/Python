a7d449c40efdc8a28a00e34afb21a5a1c170026c
ferhat-kaplan <69006650+ferhat-kaplan@users.noreply.github.com>
2020-07-31 23:25:14 +0300

    Add files via upload

---
+++ 2.Nesne_tabanli_programlama/Attributes_nitelikler.py
@@ -0,0 +1,14 @@
+class Footballer:
+    # Bunlar atttribute 'lerdir yani nitelikler
+    age = 30
+    football_clup = 'Barcelona'
+
+
+f1 = Footballer()
+
+print(f1)
+print(f1.age)
+print(f1.football_clup)
+
+f1.football_clup = 'Real madrid'
+print(f1.football_clup)
