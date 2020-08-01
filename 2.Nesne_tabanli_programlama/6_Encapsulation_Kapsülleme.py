cc0ae15ba4f28a4d85d36aa5f9dbcc5116860320
ferhat-kaplan <69006650+ferhat-kaplan@users.noreply.github.com>
2020-08-01 05:13:54 +0300

    Add files via upload

---
+++ 2.Nesne_tabanli_programlama/6_Encapsulation_Kapsülleme.py
@@ -0,0 +1,28 @@
+class BankAccount(object):
+
+    def __init__(self, name, money, address):
+        self.name = name  # Global
+        # moneyin başına __ ekleyerek dışarıdan erişimini kapatıyoruz
+        self.__money = money  # Private
+        self.address = address
+
+    # get ve set metodlarını kullanarak dışarıdan erişimi açabiliriz.
+    # get and set
+    def getMoney(self):
+        # __money bilgisini dışarı aktar.
+        return self.__money
+
+    def setMoney(self, amount):
+        # __money'e ekleme yap
+        self.__money += amount
+
+
+p1 = BankAccount('cabbar', 3000, 'hawai')
+p2 = BankAccount('mahmut', 12, 'fakirhane')
+
+# Parayı göster
+print(p1.getMoney())
+# Parayı arttır
+p1.setMoney(20000)
+# Parayı göster
+print(p1.getMoney())
